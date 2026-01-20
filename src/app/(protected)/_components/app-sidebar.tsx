7a70b822d0dafb5f19d1639603ca19f78888aa9e
TGianoni <gianoni2@hotmail.com>
2026-01-20 10:02:33 -0300

    feat: add clinic data to user session and render it on sidebar

--- src/app/(protected)/_components/app-sidebar.tsx
+++ src/app/(protected)/_components/app-sidebar.tsx
@@ -5,7 +5,7 @@ import Image from "next/image"
 import Link from "next/link"
 import { useRouter } from "next/navigation"
 
-import { Button } from "@/components/ui/button"
+import { Avatar, AvatarFallback } from "@/components/ui/avatar"
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
 import {
   Sidebar,
@@ -47,6 +47,8 @@ const items = [
 
 export function AppSidebar() {
   const router = useRouter()
+  const session = authClient.useSession()
+
   const handleSignOut = async () => {
     await authClient.signOut({
       fetchOptions: {
@@ -85,7 +87,15 @@ export function AppSidebar() {
           <SidebarMenuItem>
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
-              <Button>Clínica</Button>
+              <SidebarMenuButton size="lg">
+              <Avatar>
+                <AvatarFallback>F</AvatarFallback>
+              </Avatar>
+              <div>
+                <p className="text-sm">{session.data?.user.clinic.name}</p>
+                <p className="text-sm text-muted-foreground">{session.data?.user.email}</p>
+              </div> 
+              </SidebarMenuButton>
               </DropdownMenuTrigger>
               <DropdownMenuContent>
                 <DropdownMenuItem onClick={handleSignOut}>
