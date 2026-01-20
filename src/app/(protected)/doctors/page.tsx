260c34541e69781d3723158ee0c5640590a7334c
TGianoni <gianoni2@hotmail.com>
2026-01-20 13:43:01 -0300

    feat: ensure user has clinic on doctors page

--- src/app/(protected)/doctors/page.tsx
+++ src/app/(protected)/doctors/page.tsx
@@ -1,10 +1,23 @@
 import { Plus } from 'lucide-react'
+import { headers } from 'next/headers'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
 import { Button } from '@/components/ui/button'
 import { PageActions, PageContainer, PageContent, PageDescription, PageHeader, PageHeaderContent, PageTitle } from '@/components/ui/page-container'
+import { auth } from '@/lib/auth'
 
-const DoctorsPage = () => {
+
+const DoctorsPage = async () => {
+    const session = await auth.api.getSession({
+        headers: await headers()
+    })
+    if (!session?.user) {
+        redirect("/authentication")
+    }
+    if (!session.user.clinic) {
+        redirect("/clinic/form")
+    }
   return (
     <PageContainer>
         <PageHeader>
