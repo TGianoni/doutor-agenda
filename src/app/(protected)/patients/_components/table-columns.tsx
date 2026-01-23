e96ea7384a0c34b212c83ef7b6cd647671e51a3d
TGianoni <gianoni2@hotmail.com>
2026-01-23 16:34:54 -0300

    feat: ensure patient can be updated

--- src/app/(protected)/patients/_components/table-columns.tsx
+++ src/app/(protected)/patients/_components/table-columns.tsx
@@ -1,19 +1,11 @@
 "use client"
 
 import { ColumnDef } from "@tanstack/react-table"
-import { EditIcon, MoreVerticalIcon, TrashIcon } from "lucide-react"
 
-import { Button } from "@/components/ui/button"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
 import { patientsTable } from "@/db/schema"
 import { formatPhoneNumber } from "@/helpers/phone"
+
+import PatientsTableActions from "./table-actions"
 
 export const patientsTableColumns: ColumnDef<typeof patientsTable.$inferSelect>[] = [
   {
@@ -49,25 +41,7 @@ export const patientsTableColumns: ColumnDef<typeof patientsTable.$inferSelect>[
     cell: (params) => {
       const patient = params.row.original
         return (
-            <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                    <Button variant="ghost" size="icon">
-                        <MoreVerticalIcon className="h-4 w-4"/>
-                    </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent>
-                  <DropdownMenuLabel>{patient.name}</DropdownMenuLabel>
-                  <DropdownMenuSeparator />                    
-                    <DropdownMenuItem>
-                      <EditIcon />
-                      Editar
-                    </DropdownMenuItem>
-                    <DropdownMenuItem>
-                      <TrashIcon />
-                      Excluir
-                    </DropdownMenuItem>
-                </DropdownMenuContent>
-                </DropdownMenu>
+            <PatientsTableActions patient={patient}/>
         )
     }
   }
--- src/components/ui/dropdown-menu.tsx
+++ src/components/ui/dropdown-menu.tsx
@@ -1,8 +1,8 @@
 "use client"
 
-import * as React from "react"
 import * as DropdownMenuPrimitive from "@radix-ui/react-dropdown-menu"
 import { CheckIcon, ChevronRightIcon, CircleIcon } from "lucide-react"
+import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
@@ -155,7 +155,7 @@ function DropdownMenuLabel({
       data-slot="dropdown-menu-label"
       data-inset={inset}
       className={cn(
-        "px-2 py-1.5 text-sm font-medium data-[inset]:pl-8",
+        "px-2 py-1.5 text-sm font-medium data-inset:pl-8",
         className
       )}
       {...props}
@@ -240,18 +240,18 @@ function DropdownMenuSubContent({
 
 export {
   DropdownMenu,
-  DropdownMenuPortal,
-  DropdownMenuTrigger,
+  DropdownMenuCheckboxItem,
   DropdownMenuContent,
   DropdownMenuGroup,
-  DropdownMenuLabel,
   DropdownMenuItem,
-  DropdownMenuCheckboxItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
   DropdownMenuRadioGroup,
   DropdownMenuRadioItem,
   DropdownMenuSeparator,
   DropdownMenuShortcut,
   DropdownMenuSub,
-  DropdownMenuSubTrigger,
   DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
 }
